c621a0b176e151bee108fa22e3a0b55217d4d501
Clayton Albachten <clay.reed.a@gmail.com>
2014-10-03 14:00:35 -0400

    Play w/ object inherited properties

--- objects.js
+++ objects.js
@@ -1,8 +1,25 @@
-function Person (firstName, lastName, theAge, favoriteColor) {
+function Person (firstName, lastName, theAge, favoriteColor, living) {
   this.first = firstName;
   this.last = lastName;
   this.age = theAge;
   this.sayHello = function() {
-    console.log("Hello, I'm " + this.first + ' ' + this.last + '!'); 
+    if (this.isAlive()) {
+     console.log("Hello, I'm " + this.first + ' ' + this.last + '!'); 
+    } else {
+      console.log(this.last + " cannot speak. Is dead.");
+    }
   }; 
+  this.alive = living; 
 }
+
+Person.prototype.isAlive = function() {
+  return this.alive;
+}; 
+
+var clay = new Person("Clayton", "Albachten", 26, "blue", true);
+var cheryl = new Person("Cheryl", "Wu", 21, "red", true);
+var turing = new Person("Alan", "Turing", 102, "gray", false);
+
+clay.sayHello();
+cheryl.sayHello();
+turing.sayHello(); 
